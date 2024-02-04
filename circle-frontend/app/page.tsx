4f66b1386ece8de047a80c5f963d6e196a3cae42
Cancey Dejean <canceyd@gmail.com>
2024-02-03 19:09:27 -0600

    Add page API and controller

--- circle-frontend/app/page.tsx
+++ circle-frontend/app/page.tsx
@@ -3,8 +3,26 @@ import Questions from "@/components/Questions"
 import Reviews from "@/components/Reviews"
 import Services from "@/components/Services"
 import Syllabus from "@/components/Syllabus"
+import { config } from "@/libs/config"
 
-export default function Home() {
+const getPages = async () => {
+  const requestOptions = {
+    headers: {
+      Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+    },
+  }
+  const request = await fetch(
+    `${config.api}/api/pages?populate=*&filters[title][$eq]=home`,
+    requestOptions
+  )
+
+  const response = await request.json()
+  return response
+}
+
+export default async function Home() {
+  const pages = await getPages()
+  console.log(pages.data)
   return (
     <>
       <Hero />
